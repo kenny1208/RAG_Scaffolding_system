c23c2c06a9c3ae4d7bffc12cd9f5b2812c275226
kenny1208 <kenny1208@gmail.com>
2025-07-16 14:34:18 +0800

    fix prompt

--- app.py
+++ app.py
@@ -836,8 +836,6 @@ def generate_module_content(session_id, module, profile):
            
            如果有多個來源，請列出主要的第1個來源。
            如果超過一個頁碼，只列出第1個頁碼。
-
-        10. 【強制要求】描述現在的鷹架等級。
         
         使用markdown格式化您的內容，以提高可讀性。
         
